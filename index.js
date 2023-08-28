277dd953099fe75c7fc43f84946104bc4649660a
oigomezz <oigomezz@unal.edu.co>
2023-08-28 18:28:12 -0500

    Se agregan dos metodos get

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 1000
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 2000
+  }
+]
+
 app.get('/', (req, res) => {
   res.send('Hola mundo desde mi server en express')
+})
+
+app.get('/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params
+  const result = products.filter(item => item.id == id)
+  console.log(id, result)
+  res.json(result);
+
 })
 
 app.listen(port,()=>{
