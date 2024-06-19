c6b213de0b63bbbc2ac9be2abca1c3522451e4b6
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-19 12:12:04 -0500

    🚧 Refactored brand to use Mongoose and MongoDB, updated controller, service, and entity to reflect changes.

--- src/products/controllers/brands.controller.ts
+++ src/products/controllers/brands.controller.ts
@@ -6,12 +6,13 @@ import {
   Body,
   Put,
   Delete,
-  ParseIntPipe,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { BrandsService } from '../services/brands.service';
 import { CreateBrandDto, UpdateBrandDto } from '../dtos/brand.dtos';
 
+@ApiTags('brands')
 @Controller('brands')
 export class BrandsController {
   constructor(private brandsService: BrandsService) {}
@@ -22,7 +23,7 @@ export class BrandsController {
   }
 
   @Get(':id')
-  get(@Param('id', ParseIntPipe) id: number) {
+  get(@Param('id') id: string) {
     return this.brandsService.findOne(id);
   }
 
@@ -32,15 +33,12 @@ export class BrandsController {
   }
 
   @Put(':id')
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() payload: UpdateBrandDto,
-  ) {
+  update(@Param('id') id: string, @Body() payload: UpdateBrandDto) {
     return this.brandsService.update(id, payload);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.brandsService.remove(+id);
+  remove(@Param('id') id: string) {
+    return this.brandsService.remove(id);
   }
 }
