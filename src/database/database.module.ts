a6ba7bf550eb25aff5b1b049ec3d3f4d8d0d5dae
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-19 10:10:09 -0500

    🗃️ Added MongoDB dependency and implemented database connection in database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,23 @@
 import { Module, Global } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
 
 const API_KEY = '1234567890';
 const API_KEY_PROD = 'PROD123456';
+
+const uri =
+  'mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary';
+
+const client = new MongoClient(uri);
+
+async function run() {
+  await client.connect();
+  const database = client.db('store');
+  const taskColletion = database.collection('tasks');
+  const tasks = await taskColletion.find().toArray();
+  console.log(tasks);
+}
+
+run();
 
 @Global()
 @Module({
