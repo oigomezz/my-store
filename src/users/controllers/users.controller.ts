87b8f8fef92a9cf72b077b664cf975c9b5feb2a3
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-19 12:16:47 -0500

    🚧 Refactored user to use Mongoose and MongoDB, updated controller, service, and entity to reflect changes.

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,46 +1,55 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Post,
-  Body,
-  Put,
-  Delete,
-  ParseIntPipe,
-} from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Db } from 'mongodb';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
-import { UsersService } from '../services/users.service';
+import { User } from '../entities/user.entity';
 import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
+import { ProductsService } from '../../products/services/products.service';
 
-@Controller('users')
-export class UsersController {
-  constructor(private usersService: UsersService) {}
+@Injectable()
+export class UsersService {
+  constructor(
+    private productsService: ProductsService,
+    @Inject('MONGO') private databaseMongo: Db,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
 
-  @Get()
   findAll() {
-    return this.usersService.findAll();
+    return this.userModel.find().exec();
   }
 
-  @Get(':id')
-  get(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findOne(id);
+  getTasks() {
+    const tasksCollection = this.databaseMongo.collection('tasks');
+    return tasksCollection.find().toArray();
   }
 
-  @Post()
-  create(@Body() payload: CreateUserDto) {
-    return this.usersService.create(payload);
+  async findOne(id: string) {
+    return this.userModel.findById(id);
   }
 
-  @Put(':id')
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() payload: UpdateUserDto,
-  ) {
-    return this.usersService.update(id, payload);
+  async getOrdersByUser(userId: string) {
+    const user = await this.findOne(userId);
+    return {
+      date: new Date(),
+      user,
+      // products: this.productsService.findAll(),
+      products: [],
+    };
   }
 
-  @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.remove(+id);
+  create(data: CreateUserDto) {
+    const newModel = new this.userModel(data);
+    return newModel.save();
+  }
+
+  update(id: string, changes: UpdateUserDto) {
+    return this.userModel
+      .findByIdAndUpdate(id, { $set: changes }, { new: true })
+      .exec();
+  }
+
+  remove(id: string) {
+    return this.userModel.findByIdAndDelete(id);
   }
 }
