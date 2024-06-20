233711ca4534429046cc4a2d65e0c03457673fac
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-20 10:36:14 -0500

    🔐 Switch from MongoDB to PostgreSQL, update environment variables and database configuration

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,10 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MongoClient } from 'mongodb';
 
 import config from '../config';
-
-const API_KEY = '1234567890';
-const API_KEY_PROD = 'PROD123456';
-
 @Global()
 @Module({
   imports: [
@@ -28,7 +24,10 @@ const API_KEY_PROD = 'PROD123456';
   providers: [
     {
       provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+      useValue:
+        process.env.NODE_ENV === 'prod'
+          ? process.env.API_KEY_PROD
+          : process.env.API_KEY,
     },
     {
       provide: 'MONGO',
