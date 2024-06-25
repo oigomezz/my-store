a35ce1c52b9bde199c2ad1e17fe427def25a7c37
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-25 09:53:40 -0500

    🚧 Added ApiKeyGuard to AppController, created Public decorator, and modified ApiKeyGuard to respect Public decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { Public } from './auth/decorators/public.decorator';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,6 +11,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Public()
+  @Get('new')
+  newEndpoint() {
+    return 'Soy un nuevo endpoint';
   }
 
   @Get('/tasks/')
--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -4,19 +4,27 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
-
 import { Request } from 'express';
+
+import { IS_PUBLIC_KEY } from './../decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Auth');
     const isAuth = authHeader === '1234';
     if (!isAuth) throw new UnauthorizedException('not allow');
+
     return isAuth;
   }
 }
