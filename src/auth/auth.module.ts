49df7b0bcb76742122cb72b7c5632c88d6b1d4f0
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-25 10:55:25 -0500

    ➕ Added @nestjs/jwt and passport-jwt dependencies, updated auth module and service to use JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
-import { UsersModule } from './../users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { UsersModule } from './../users/users.module';
 
 @Module({
   imports: [UsersModule, PassportModule],
