8564f4a1e043f706edc1ec46ee044278c9f667cc
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-26 16:01:37 -0500

    🚧 Added ordersByCustomer method to OrdersService and ProfileController to UsersModule

--- src/users/services/orders.service.ts
+++ src/users/services/orders.service.ts
@@ -2,12 +2,16 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import { Customer } from '../entities/customer.entity';
 import { Order } from '../entities/order.entity';
 import { CreateOrderDto, UpdateOrderDto } from '../dtos/order.dto';
 
 @Injectable()
 export class OrdersService {
-  constructor(@InjectModel(Order.name) private orderModel: Model<Order>) {}
+  constructor(
+    @InjectModel(Customer.name) private customerModel: Model<Customer>,
+    @InjectModel(Order.name) private orderModel: Model<Order>,
+  ) {}
 
   findAll() {
     return this.orderModel
@@ -15,6 +19,14 @@ export class OrdersService {
       .populate('customer')
       .populate('products')
       .exec();
+  }
+
+  async ordersByCustomer(customerId: number) {
+    const customer = await this.customerModel.findOne({
+      where: { id: customerId },
+    });
+
+    return this.orderModel.find({ where: { customer } });
   }
 
   async findOne(id: string) {
